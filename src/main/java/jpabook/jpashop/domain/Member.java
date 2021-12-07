d6049093d6b1a3a3abcbacb0109faad1eeae35e8
yskim621 <yskim62100@gmail.com>
2021-12-08 00:02:52 +0900

    Updating Member Entity

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +15,8 @@ public class Member {
     @Id @GeneratedValue
     @Column(name = "member_id")
     private Long id;
+
+    @NotEmpty // [View + Controller] = presentation 계층에 validation 이 들어가는 것은 권장하지 않음
     private String name;
 
     @Embedded
