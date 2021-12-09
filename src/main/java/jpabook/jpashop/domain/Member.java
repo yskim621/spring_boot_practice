7e56f55c4abba397870b0d6131304ca9cb50306c
yskim621 <yskim62100@gmail.com>
2021-12-09 10:04:03 +0900

    Updating Member Entity

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -22,6 +23,7 @@ public class Member {
     @Embedded
     private Address address;
 
+    @JsonIgnore //- presentation layer를 위한 로직이 추가되는 형태이므로 권장하지 않음
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 }
